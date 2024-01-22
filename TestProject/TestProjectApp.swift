c6a581ea7fc607e32d7704cada4f0d9c4046eab4
Derick <derick.derick@rakuten.com>
2024-01-22 20:39:30 +0800

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Derick, Derick | RASIA on 22/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
